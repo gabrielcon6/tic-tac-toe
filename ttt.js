158387cb6ea01f3f68a07d34f52ed47a3170886f
Gabriel Conceicao <gabrielcon6cao@gmail.com>
2019-09-11 06:42:21 +1000

    updated files 3

--- ttt.js
+++ ttt.js
@@ -1,3 +1,7 @@
+let turnsCount = 0;
+let cellTurn;
+let thisIndex;
+
 let cellsLocation = {
     cell1Location: $('.cell1'),
     cell2Location: $('.cell2'),
@@ -10,161 +14,124 @@ let cellsLocation = {
     cell9Location: $('.cell9')
 };
 
-let xOrOCells;
-
-let xCells = {
-    cell1Element: false,
-    cell2Element: false,
-    cell3Element: false,
-    cell4Element: false,
-    cell5Element: false,
-    cell6Element: false,
-    cell7Element: false,
-    cell8Element: false,
-    cell9Element: false
-};
-
-let oCells = [{
-    cell1Element: true,
-    cell2Element: true,
-    cell3Element: true,
-    cell4Element: false,
-    cell5Element: false,
-    cell6Element: false,
-    cell7Element: false,
-    cell8Element: false,
-    cell9Element: false
+let xCells = [{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
 }];
 
-const checkingCellsValues = function () {
-    for (const i in cellsLocation) {
-        if (cellsLocation[i].val() === 'X') { //does it really get the value
-            for (const j in xCells)
-                xCells[j] = true;
-        } else if (cellsLocation[i].val() === 'O') {
-            for (const j in oCells)
-                oCells[j] = true;
+let oCells = [{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+},
+{
+    cellElement: false
+}
+];
+
+const checkingXCellsValues = function () {
+    for (let i = 0; i < xCells.length; i++) {
+        if (thisIndex == i) {
+            xCells[i].cellElement = true;
+            winnerCheck(xCells, 'X');
         }
     }
-    updatingXorOCellsToX();
 };
 
-const updatingXorOCellsToX = function () {
-    xOrOCells = [];
-    xOrOCells.push(xCells);
-    winnerCheck();
+const checkingOCellsValues = function () {
+    for (let i = 0; i < oCells.length; i++) {
+        if (thisIndex == i) {
+            oCells[i].cellElement = true;
+            winnerCheck(oCells, 'O');
+        }
+    }
 };
 
-const updatingXorYCellsToO = function () {
-    xOrOCells = [];
-    xOrOCells.push(oCells);
-    winnerCheck();
-};
-
-const winnerCheck = function () {
-    if ((xOrOCells[0].cell1Element && xOrOCells[0].cell2Element && xOrOCells[0].cell3Element) ||
-        (xOrOCells[0].cell1Element && xOrOCells[0].cell4Element && xOrOCells[0].cell7Element) ||
-        (xOrOCells[0].cell1Element && xOrOCells[0].cell5Element && xOrOCells[0].cell9Element) ||
-        (xOrOCells[0].cell2Element && xOrOCells[0].cell5Element && xOrOCells[0].cell8Element) ||
-        (xOrOCells[0].cell3Element && xOrOCells[0].cell5Element && xOrOCells[0].cell7Element) ||
-        (xOrOCells[0].cell3Element && xOrOCells[0].cell6Element && xOrOCells[0].cell9Element) ||
-        (xOrOCells[0].cell4Element && xOrOCells[0].cell5Element && xOrOCells[0].cell6Element) ||
-        (xOrOCells[0].cell7Element && xOrOCells[0].cell8Element && xOrOCells[0].cell9Element)
+const winnerCheck = function (value, winner) {
+    if ((value[0].cellElement && value[1].cellElement && value[2].cellElement) ||
+        (value[0].cellElement && value[3].cellElement && value[6].cellElement) ||
+        (value[0].cellElement && value[4].cellElement && value[8].cellElement) ||
+        (value[1].cellElement && value[4].cellElement && value[7].cellElement) ||
+        (value[2].cellElement && value[4].cellElement && value[6].cellElement) ||
+        (value[2].cellElement && value[5].cellElement && value[8].cellElement) ||
+        (value[3].cellElement && value[4].cellElement && value[5].cellElement) ||
+        (value[6].cellElement && value[7].cellElement && value[8].cellElement)
     ) {
-        alert(`${xOrOCells[0]} is the winner`);
-        console.log(`${xOrOCells[0]} is the winner`);
-    }
-    else {
-        updatingXorYCellsToO();
+        alert(`${winner} is the winner`);
+        resetTrueOrFalse();
     }
 };
 
-
-//THIS IS MY OLD FUNCTION WITH FOR LOOP INSTEAD
-
-// const cellsCheck = function () {
-//     for (let index = 0; index < xOrYCells.length; index++) {
-// if (cell1Element[i] === 'x') {
-//     xOrYCells[0] === true;
-// }
-//         if ((xOrYCells[0].cell1Element && xOrYCells[0].cell2Element && xOrYCells[0].cell3Element) ||
-//             (xOrYCells[0].cell1Element && xOrYCells[0].cell4Element && xOrYCells[0].cell7Element) ||
-//             (xOrYCells[0].cell1Element && xOrYCells[0].cell5Element && xOrYCells[0].cell9Element) ||
-//             (xOrYCells[0].cell2Element && xOrYCells[0].cell5Element && xOrYCells[0].cell8Element) ||
-//             (xOrYCells[0].cell3Element && xOrYCells[0].cell5Element && xOrYCells[0].cell7Element) ||
-//             (xOrYCells[0].cell3Element && xOrYCells[0].cell6Element && xOrYCells[0].cell9Element) ||
-//             (xOrYCells[0].cell4Element && xOrYCells[0].cell5Element && xOrYCells[0].cell6Element) ||
-//             (xOrYCells[0].cell7Element && xOrYCells[0].cell8Element && xOrYCells[0].cell9Element)
-//         ) {
-//             console.log(`${xOrYCells[0]} is the winner`);
-//         }
-//         else {
-//             tie();
-//         }
-//     }
-// };
-
-
-let turnsCount = 0;
-let cellTurn;
-
-const xOrOPrint = function () {
-    // if (cellTurn.val() === 'X' || 'O') {
-    //     alert(`You can't select this cell twice`);
-    // }
-    // else
-    if (turnsCount % 2 === 0) {
-        cellTurn.text('X');
-        turnsCount++;
-    } else {
-        cellTurn.text('O');
-        turnsCount++;
-        if (turnsCount === 10) {
-            turnsCount++;
-            alert(`DRAW!`);
-        }
-    }
+const resetTrueOrFalse = function () {
+    for (let i = 0; i < xCells.length; i++) {
+        xCells[i].cellElement = false;
+    } for (let i = 0; i < oCells.length; i++) {
+        oCells[i].cellElement = false;
+    } 
 }
 
+const xOrOPrint = function () {
+    if (turnsCount % 2 == 0) {
+        cellTurn.text('X');
+        turnsCount++;
+        checkingXCellsValues();
+    } else if (turnsCount % 2 !== 0) {
+        cellTurn.text('O');
+        turnsCount++;
+        checkingOCellsValues();
+    } else if (turnsCount === 8) {
+        alert(`DRAW!`);
+    }
+};
 
 $(document).ready(function () {
 
-    $('.cell1').click(function () {
+    $('.cell').one('click', function () {
         cellTurn = $(this);
+        thisIndex = $(this).attr("data-id");
         xOrOPrint();
     })
-    $('.cell2').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell3').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell4').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell5').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell6').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell7').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell8').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-    $('.cell9').click(function () {
-        cellTurn = $(this);
-        xOrOPrint();
-    })
-
 
 });  
